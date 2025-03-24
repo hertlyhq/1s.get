9f504ee093b823b35b2924347631a441d4fd4e8f
Edilson Nunez <edisoft.00@gmail.com>
2025-03-23 23:45:28 -0500

    Initial commit

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Install git and deno
+apk add git deno
+
+# Check if the installation was successful
+if [ $? -ne 0 ]; then
+  echo "Error installing git and deno"
+  exit 1
+fi
+
+# Remove the /1s directory if it exists
+rm -rf /1s
+
+# Clone the repository
+git clone https://${GH_TOKEN}@github.com/HertlyHQ/1s -b main /1s
+
+# Check if the cloning was successful
+if [ $? -ne 0 ]; then
+  echo "Error cloning repository"
+  exit 1
+fi
+
+# Change the working directory
+cd /1s
+
+# Run the deno application
+deno run main
+
+# Check if the deno application ran successfully
+if [ $? -ne 0 ]; then
+  echo "Error running deno main"
+  exit 1
+fi
+
+echo "Script executed successfully"
